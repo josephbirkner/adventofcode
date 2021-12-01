2b6c9bd81c136f613d7098c242f652ead7410d05
Joseph Birkner <j.birkner@klebert-engineering.de>
2021-12-02 00:00:00 +0100

    Day 2

---
+++ 21/2.py
@@ -0,0 +1,19 @@
+x = 0
+y = 0
+aim = 0
+with open("2.data", 'r') as f:
+    for command_and_arg in f:
+        command, arg = command_and_arg.strip().split(" ")
+        arg_n = int(arg)
+        if command == "up":
+            aim -= arg_n
+        elif command == "down":
+            aim += arg_n
+        elif command == "forward":
+            x += arg_n
+            y += aim * arg_n
+            assert y >= 0
+        else:
+            print("ERROR: Command is", command, arg)
+            exit(1)
+print(x*y)
