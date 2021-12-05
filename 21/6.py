8e26e520abacf81c1c3b03cd4848352b14e555aa
Joseph Birkner <j.birkner@klebert-engineering.de>
2021-12-06 00:00:00 +0100

    Day 6

---
+++ 21/6.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+fish = [0]*9
+with open("6.data") as f:
+    for age in map(int, f.read().split(",")):
+        fish[age] += 1
+
+for _ in range(256):
+    new_fish = fish[0]
+    fish = fish[1:] + [0]
+    fish[8] += new_fish
+    fish[6] += new_fish
+
+print(sum(fish))
