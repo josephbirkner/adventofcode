eea5ae70667b2fd520f1e320013eac921708f1de
Joseph Birkner <j.birkner@klebert-engineering.de>
2022-12-10 00:58:32 +0100

    Day 9

---
+++ 22/9.1.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import numpy as np
+
+
+head_pos = np.array([0, 0])
+tail_pos = np.array([0, 0])
+
+instr_map = {
+    "R": np.array([1, 0]),
+    "L": np.array([-1, 0]),
+    "U": np.array([0, -1]),
+    "D": np.array([0, 1]),
+}
+
+visited_positions = set()
+visited_positions.add((0, 0))
+
+with open(Path(__file__).parent/"9.txt") as f:
+    for line in f:
+        line = line.strip()
+        direction = instr_map[line[0]]
+        steps = int(line[2:])
+        while steps > 0:
+            steps -= 1
+            head_pos += direction
+            dist = head_pos - tail_pos
+            abs_dist = np.abs(dist)
+            sum_dist = np.sum(abs_dist)
+            if sum_dist < 2:
+                continue
+            if np.all(np.equal(abs_dist, 1)):
+                continue
+            dist[dist < 0] = -1
+            dist[dist > 0] = 1
+            tail_pos += dist
+            visited_positions.add(tuple(tail_pos))
+            assert np.all(np.less(np.abs(head_pos - tail_pos), 2))
+
+print(len(visited_positions))
+
---
+++ 22/9.2.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import numpy as np
+
+
+knot_pos = np.zeros((10, 2))
+
+instr_map = {
+    "R": np.array([1, 0]),
+    "L": np.array([-1, 0]),
+    "U": np.array([0, -1]),
+    "D": np.array([0, 1]),
+}
+
+visited_positions = set()
+visited_positions.add((0, 0))
+
+with open(Path(__file__).parent/"9.txt") as f:
+    for line in f:
+        line = line.strip()
+        direction = instr_map[line[0]]
+        steps = int(line[2:])
+        while steps > 0:
+            steps -= 1
+            knot_pos[0] += direction
+            for i in range(1, len(knot_pos)):
+                dist = knot_pos[i-1] - knot_pos[i]
+                abs_dist = np.abs(dist)
+                sum_dist = np.sum(abs_dist)
+                if sum_dist < 2:
+                    continue
+                if np.all(np.equal(abs_dist, 1)):
+                    continue
+                dist[dist < 0] = -1
+                dist[dist > 0] = 1
+                knot_pos[i] += dist
+                assert np.all(np.less(np.abs(knot_pos[i-1] - knot_pos[i]), 2))
+            visited_positions.add(tuple(knot_pos[9]))
+
+print(len(visited_positions))
+
